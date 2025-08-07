7098aefee48ee1757e8befdac892238a2c0f4248
JesusNeyr <jesua.park@gmail.com>
2025-08-06 21:59:23 -0300

    fix: usamos 0.0.0.0 para railway

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Escuchar en 0.0.0.0 para que funcione en Railway
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
